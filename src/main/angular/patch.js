829a36d94e1b3427e3e2b1e6fc8e78fa0455c36c
przMPDL <wurzelpuzz@users.noreply.github.com>
2021-10-15 10:15:40 +0200

    Angular 12 prepare

--- src/main/angular/patch.js
+++ src/main/angular/patch.js
@@ -1,13 +1,64 @@
+const path = require('path');
 const fs = require('fs');
-const f = 'node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js';
+const fsp = require('fs/promises');
 
-fs.readFile(f, 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true}');
+const file2replace = path.join(__dirname,'node_modules','@angular-devkit','build-angular','src','webpack','configs','browser.js');
+const filename = path.basename(file2replace);
+const replacement = path.join(__dirname, 'src','assets','webpack','patch', filename);
+const backup_target = path.join(__dirname, 'src','assets','webpack','backup');
 
-  fs.writeFile(f, result, 'utf8', function (err) {
-    if (err) return console.log(err);
+const existsOrCreate = (folder_path) => {
+  return new Promise((resolve, reject) => {
+      fs.stat(folder_path, (err) => {
+          if (err) {
+              if (err.code === 'ENOENT') {
+                  fs.mkdir(folder_path, {recursive: true}, (err) => {
+                      if (err) {
+                          reject(err);
+                      } else {
+                          resolve(folder_path);
+                      }
+                  });
+              } else {
+                  reject(err);
+              }
+          } else {
+              resolve(folder_path);
+          }
+      });
   });
-});
+}
+
+const existsOrTerminate = (file_path) => {
+  return new Promise((resolve, reject) => {
+      fs.stat(file_path, err => {
+          if (err) {
+              if (err.code === 'ENOENT') {
+                  resolve(false);
+              } else {
+                  reject(err);
+              }
+          } else {
+              resolve(true);
+          }
+      });
+  });
+}
+
+const patch = async () => {
+  try {
+      const backup = await existsOrCreate(backup_target);
+      await fsp.copyFile(file2replace, path.resolve(backup, filename));
+      const replace = await existsOrTerminate(replacement);
+      if (replace) {
+          await fsp.copyFile(replacement, file2replace);
+          return 'DONE!';
+      } else {
+          return 'MISSING REPLACEMENT!';
+      }
+  } catch (e) {
+      throw e;
+  }
+}
+
+patch().then(console.log).catch(err => console.log(err));
