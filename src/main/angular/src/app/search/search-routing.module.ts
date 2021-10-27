cdbb357abd64e2ca10bed64ffa932f03ad7a3e70
przMPDL <wurzelpuzz@users.noreply.github.com>
2021-10-27 14:26:36 +0200

    routing stuff

--- src/main/angular/src/app/search/search-routing.module.ts
+++ src/main/angular/src/app/search/search-routing.module.ts
@@ -10,15 +10,11 @@ import {AdminGuard} from '../base/services/admin-guard.service';
 const routes: Routes = [
   {path: 'search', component: SearchComponent,
     children: [
-      {path: '',
-        children: [
-          {path: 'users', component: UserSearchComponent, canActivate: [AdminGuard]},
-          {path: 'organizations', redirectTo: '/organizations', pathMatch: 'full'},
-          {path: 'contexts', redirectTo: '/contexts', pathMatch: 'full'},
-          {path: 'items', component: ItemSearchComponent},
-          {path: '', component: ContextSearchComponent}, // Default route -> Dummy Info Seite (TODO: sollte irgendwann durch eine echte ContextSearch oder eine dedizierte Hinweis-Seite ersetzt werden)
-        ],
-      },
+      {path: 'users', component: UserSearchComponent, canActivate: [AdminGuard]},
+      {path: 'organizations', redirectTo: '/organizations', pathMatch: 'full'},
+      {path: 'contexts', redirectTo: '/contexts', pathMatch: 'full'},
+      {path: 'items', component: ItemSearchComponent},
+      {path: '', component: ContextSearchComponent}, // Default route -> Dummy Info Seite (@TODO: sollte irgendwann durch eine echte ContextSearch oder eine dedizierte Hinweis-Seite ersetzt werden)
     ],
   },
 ];
