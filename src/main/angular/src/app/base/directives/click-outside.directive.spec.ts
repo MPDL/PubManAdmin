10237f1d4778dcb58dab41399e74c6c2b975d661
przMPDL <wurzelpuzz@users.noreply.github.com>
2021-11-06 10:28:40 +0100

    no tests

--- src/main/angular/src/app/base/directives/click-outside.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {ClickOutsideDirective} from './click-outside.directive';
-
-describe('ClickOutsideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickOutsideDirective(null);
-    expect(directive).toBeTruthy();
-  });
-});
