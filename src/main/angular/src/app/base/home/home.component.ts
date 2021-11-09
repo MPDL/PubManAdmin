69e39de9664669b445b55a7a407348eb81f81873
przMPDL <wurzelpuzz@users.noreply.github.com>
2021-11-09 09:21:54 +0100

    navigation stuff

--- src/main/angular/src/app/base/home/home.component.ts
+++ src/main/angular/src/app/base/home/home.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {MessagesService} from '../services/messages.service';
-import {AuthenticationService} from '../services/authentication.service';
 import {ConnectionService} from '../services/connection.service';
 import {Subscription} from 'rxjs';
 
@@ -10,37 +8,18 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  loginSubscription: Subscription;
   hostSubscription: Subscription;
-
-  isLoggedIn: boolean = false;
   hostName: string;
 
   constructor(
     private connectionService: ConnectionService,
-    private login: AuthenticationService,
-    private messagesService: MessagesService
   ) {}
 
   ngOnInit() {
-    this.loginSubscription = this.login.isLoggedIn$.subscribe((data) => this.isLoggedIn = data);
     this.hostSubscription = this.connectionService.connectionService.subscribe((data) => this.hostName = data);
   }
 
   ngOnDestroy() {
-    this.loginSubscription.unsubscribe();
     this.hostSubscription.unsubscribe();
-  }
-
-  connect() {
-    if (!this.isLoggedIn) {
-      const baseUrl = prompt('URL to connect 2', 'https://');
-      if (baseUrl === null) {
-        return;
-      }
-      this.connectionService.setConnection(baseUrl);
-    } else {
-      this.messagesService.warning('you MUST logout, in order 2 change the connection!');
-    }
   }
 }
--- src/main/angular/src/app/base/navigation/navigation.component.ts
+++ src/main/angular/src/app/base/navigation/navigation.component.ts
@@ -1,12 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
   selector: 'navigation-component',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
 })
-export class NavigationComponent implements OnInit {
-  constructor() {}
+export class NavigationComponent implements OnInit, OnDestroy {
+  adminSubscription: Subscription;
+  isAdmin: boolean;
 
-  ngOnInit() {}
+  constructor(
+    private authenticationService: AuthenticationService,
+  ) {}
+
+  ngOnInit() {
+    this.adminSubscription = this.authenticationService.isAdmin$.subscribe((data) => this.isAdmin = data);
+  }
+
+  ngOnDestroy() {
+    this.adminSubscription.unsubscribe();
+  }
 }
