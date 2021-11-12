e5683a7ac3a7c80f46fee7ad9ba9444086a6fa1e
przMPDL <wurzelpuzz@users.noreply.github.com>
2021-11-12 15:50:56 +0100

    directives not needed

--- src/main/angular/src/app/base/common/shared.module.ts
+++ src/main/angular/src/app/base/common/shared.module.ts
@@ -2,10 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import {SelectedItemColourDirective} from '../directives/selected-item-colour.directive';
-import {ClickOutsideDirective} from '../directives/click-outside.directive';
-import {DocumentClickDirective} from '../directives/documentclick.directive';
-import {ValueNotAllowedDirective} from '../directives/value-not-allowed.directive';
 import {NgxPaginationModule} from 'ngx-pagination';
 import {SearchTermComponent} from '../common/components/search-term/search-term.component';
 import {SearchService} from '../common/services/search.service';
@@ -19,18 +15,10 @@ import {SuggestionComponent} from './components/suggestion/suggestion.component'
     ReactiveFormsModule,
   ],
   declarations: [
-    SelectedItemColourDirective,
-    ClickOutsideDirective,
-    DocumentClickDirective,
-    ValueNotAllowedDirective,
     SearchTermComponent,
     SuggestionComponent,
   ],
   exports: [
-    SelectedItemColourDirective,
-    ClickOutsideDirective,
-    DocumentClickDirective,
-    ValueNotAllowedDirective,
     NgxPaginationModule,
     SearchTermComponent,
     SuggestionComponent,
