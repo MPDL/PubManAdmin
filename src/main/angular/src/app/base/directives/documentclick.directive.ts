423f503bfc23b9bec1c13fc73677adb39af1888f
przMPDL <wurzelpuzz@users.noreply.github.com>
2021-11-15 13:43:29 +0100

    Revert "directives not needed"
    This reverts commit e5683a7ac3a7c80f46fee7ad9ba9444086a6fa1e.

---
+++ src/main/angular/src/app/base/directives/documentclick.directive.ts
@@ -0,0 +1,20 @@
+import {Directive, ElementRef, HostListener, Output, EventEmitter} from '@angular/core';
+
+@Directive({
+  selector: '[clickOutside]',
+})
+export class DocumentClickDirective {
+  @Output()
+    clickOutside: EventEmitter<any> = new EventEmitter();
+
+  constructor(
+    private _elementRef: ElementRef
+  ) {}
+
+  @HostListener('document:click', ['$event.target']) onMouseEnter(targetElement) {
+    const clickedInside = this._elementRef.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.clickOutside.emit(null);
+    }
+  }
+}
