d74330ba46cab6e59cfca23edde5149685b116a6
przMPDL <wurzelpuzz@users.noreply.github.com>
2022-02-16 08:45:22 +0100

    version stuff

--- src/main/angular/src/app/base/footer/footer.component.ts
+++ src/main/angular/src/app/base/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { Subscription } from 'rxjs';
+import {Subscription} from 'rxjs';
 import {ConnectionService} from '../services/connection.service';
 
 const {version: appVersion} = require('../../../../package.json');
